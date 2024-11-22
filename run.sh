9374ce8e9dc455008c63b3fb6e4ae07cdb8db640
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-11-22 09:15:32 -0500

    Amplitude series.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-for i in {001..026} ; do sbatch  sbatch_scripts/"sbatch_FeGaB_koerner_01_B_series_$i.sh" ; done
+for i in {001..020} ; do sbatch sbatch_scripts/"sbatch_FeGaB_koerner_01_amp_series_$i.sh" ; done
