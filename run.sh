7b63015912733a8856b5deec96c97b24dac47be7
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-11-22 17:12:49 -0500

    Fix typo and re-run.

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-for i in {001..020} ; do sbatch sbatch_scripts/"sbatch_FeGaB_koerner_01_amp_series_$i.sh" ; done
+#for i in {001..020} ; do sbatch sbatch_scripts/"sbatch_FeGaB_koerner_01_amp_series_$i.sh" ; done
+sbatch sbatch_scripts/sbatch_FeGaB_koerner_01_B_series_011.sh
