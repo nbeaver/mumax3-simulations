ab7cbdaebcb6b1a36c2b117a54841f55a2012816
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-11-27 09:56:43 -0500

    Use the much more succinct Python wrapper method for another amplitude series.

---
+++ python_wrapper/run_koerner_step_amp_series.py
@@ -0,0 +1,131 @@
+import numpy as np
+from mumax_python_helpers import *
+import os
+import shutil
+os.environ['PATH'] += ":/work/sglabfiles/software/mumax"
+import time
+import sys
+
+index = int(sys.argv[1])
+amp = float(sys.argv[2])
+#amp = 7e-4
+print("index = '{}'".format(index))
+print("amp = '{}'".format(amp))
+## THIS IS WHERE THE DATA WILL BE MOVED TO AFTER SAVING
+working_dir = "/work/sglabfiles/nathaniel/mumax3-simulations/amp_series_out"
+os.chdir(working_dir)
+savedir = "/work/sglabfiles/nathaniel/mumax3-simulations/my_mx3_files"
+simname = "FeGaB_koerner_02_amp_series_{:03d}".format(index)
+
+
+# MATERIAL/SYSTEM PARAMETERS
+freq = 1425.0e6
+Bx    = 12.0e-4        # Bias field along the x direction
+Aex    = 14.9e-12     # exchange constant
+Msat  = 1030e3      # saturation magnetization
+alpha = 0.02       # damping parameter
+Ku1   = 1035 # uniaxial anisotropy
+
+# Note that this is a format string, this means that the statements inside the 
+# curly brackets get evaluated by python. In this way, we insert the values of 
+# the variables above in the script.
+script=f"""
+f 		:= {freq}												// excitation freq				[Hz]
+bstat 	:= {Bx}												// static field					[T]
+amp 	:= {amp}												// excitation amplitude 		[T_p]
+
+N := 256														// number of cells
+c := 20e-9														// cell width					[m]
+d := 20e-9														// cell height					[m]
+setgridsize(N, N, 1)	
+setcellsize(c, c, d)
+//setpbc(50,50,0)
+
+// average FeGaB magnetic parameters set for region 0
+alpha 	= {alpha}
+Ku1   	= {Ku1}													// uniaxial anisotropy			[J/m^3]
+Aex   	= {Aex}												// exchange energy				[J/m]		http://www.cwscholz.net/projects/diss/html/node58.html
+Msat  	= {Msat}													// saturation magnetization 	[A/m]
+
+// define regions
+defregion(1, xrange(0, inf))  // left half
+defregion(2, xrange(-inf, 0)) // right half
+
+// set parameters for region 1
+Ku1.setregion(1, Ku1.GetRegion(0))
+anisU.setRegion(1, vector(1, 0, 0))
+Msat.SetRegion(1, Msat.GetRegion(0)*0.8)
+alpha.SetRegion(1, alpha.GetRegion(0))	
+Aex.SetRegion(1, Aex.GetRegion(0))	
+
+// set parameters for region 2
+Ku1.setregion(2, Ku1.GetRegion(0))
+anisU.setRegion(2, vector(1, 0, 0))
+Msat.SetRegion(2, Msat.GetRegion(0))
+alpha.SetRegion(2, alpha.GetRegion(0))	
+Aex.SetRegion(2, Aex.GetRegion(0))	
+			
+// save starting conditions as .ovf
+save(regions)
+save(Ku1)
+save(AnisU)
+save(Msat)
+save(exchCoupling)
+
+
+// save starting conditions as .png
+Snapshotas(regions,"regions_init.png")
+Snapshotas(AnisU,"AnisU_init.png")
+Snapshotas(alpha,"alpha_init.png")
+Snapshotas(Ku1,"Ku1_init.png")
+Snapshotas(Msat,"Msat_init.png")
+
+
+// relax to static field
+// static field in x direction
+B_ext = vector(bstat, 0, 0)			
+
+m = uniform(1,0,0)		//.Add(0.1, randomMag());
+
+// remove surface charges left/right
+BoundaryRegion := 0
+MagLeft        := 1
+MagRight       := 1
+ext_rmSurfaceCharge(BoundaryRegion, MagLeft, MagRight)
+
+// relax M to x direction											
+run(10e-9)		
+
+saveas(m,"m_relax.ovf")
+Snapshotas(m,"m_relax.png")
+
+// static + excitation field
+B_ext = vector(bstat, amp*sin(2*pi*f*t), 0)
+// settling time
+run(200e-9)														
+
+//Simulation Time
+points 		:= 8192								
+tstep 		:= 12.5e-12											// 10ns -> largest fft-freq = 5GHz					12.5e-12s -> 40GHz
+simtime 	:= tstep * points
+
+//TableAdd(Crop(B_ext, 64, 65, 64, 65, 0, 1))					// cols 11,12,13 = B_ext
+//TableAutoSave(tstep)
+
+//save m_full as .ovf
+autosave(m_full,tstep)	
+
+// limit max solver step to avoid missing points in fft
+maxdt = 1e-12												
+
+// run simulation
+run(simtime)
+"""
+
+start_time = time.perf_counter() 
+table, fields = run_mumax3(script,simname)
+end_time = time.perf_counter()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time} seconds")
+#if savedir != '.':
+#    shutil.move(simname + '.out', savedir)
--- run.sh
+++ run.sh
@@ -1,5 +1,52 @@
 #! /bin/bash
 
-#for i in {001..020} ; do sbatch sbatch_scripts/"sbatch_FeGaB_koerner_01_amp_series_$i.sh" ; done
-#sbatch sbatch_scripts/sbatch_FeGaB_koerner_01_B_series_011.sh
-sbatch sbatch_scripts/sbatch_FeGaB_koerner_01_B_series_007.sh
+sbatch -J 00_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh  0 0.01000
+sbatch -J 01_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh  1 0.01151
+sbatch -J 02_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh  2 0.01326
+sbatch -J 03_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh  3 0.01526
+sbatch -J 04_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh  4 0.01758
+sbatch -J 05_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh  5 0.02024
+sbatch -J 06_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh  6 0.02330
+sbatch -J 07_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh  7 0.02683
+sbatch -J 08_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh  8 0.03089
+sbatch -J 09_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh  9 0.03556
+sbatch -J 10_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 10 0.04095
+sbatch -J 11_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 11 0.04715
+sbatch -J 12_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 12 0.05429
+sbatch -J 13_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 13 0.06251
+sbatch -J 14_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 14 0.07197
+sbatch -J 15_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 15 0.08286
+sbatch -J 16_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 16 0.09541
+sbatch -J 17_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 17 0.10985
+sbatch -J 18_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 18 0.12649
+sbatch -J 19_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 19 0.14563
+sbatch -J 20_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 20 0.16768
+sbatch -J 21_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 21 0.19307
+sbatch -J 22_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 22 0.22230
+sbatch -J 23_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 23 0.25595
+sbatch -J 24_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 24 0.29471
+sbatch -J 25_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 25 0.33932
+sbatch -J 26_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 26 0.39069
+sbatch -J 27_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 27 0.44984
+sbatch -J 28_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 28 0.51795
+sbatch -J 29_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 29 0.59636
+sbatch -J 30_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 30 0.68665
+sbatch -J 31_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 31 0.79060
+sbatch -J 32_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 32 0.91030
+sbatch -J 33_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 33 1.04811
+sbatch -J 34_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 34 1.20679
+sbatch -J 35_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 35 1.38950
+sbatch -J 36_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 36 1.59986
+sbatch -J 37_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 37 1.84207
+sbatch -J 38_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 38 2.12095
+sbatch -J 39_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 39 2.44205
+sbatch -J 40_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 40 2.81177
+sbatch -J 41_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 41 3.23746
+sbatch -J 42_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 42 3.72759
+sbatch -J 43_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 43 4.29193
+sbatch -J 44_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 44 4.94171
+sbatch -J 45_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 45 5.68987
+sbatch -J 46_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 46 6.55129
+sbatch -J 47_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 47 7.54312
+sbatch -J 48_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 48 8.68511
+sbatch -J 49_FeGaB_amp_series sbatch_scripts/sbatch_FeGaB_koerner_02_amp_series.sh 49 10.00000
