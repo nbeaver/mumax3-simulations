9554f2235535f214ef75c597dabd1dd9fa7975c5
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-11-27 12:29:07 -0500

    Prepare for averaging new output files.

--- analysis/run.sh
+++ analysis/run.sh
@@ -1,14 +1,5 @@
 #! /bin/bash
 
-#sbatch ../sbatch_scripts/sbatch_run_python_script.sh --debug ../my_mx3_files/FeGaB_koerner_01_B_series_001.out/ B_series_01/
-#for i in {001..026} ; do sbatch ../sbatch_scripts/sbatch_run_python_script.sh ../my_mx3_files/FeGaB_koerner_01_B_series_${i}.out B_series_01/ ; done
-#for i in {001..020} ; do sbatch ../sbatch_scripts/sbatch_run_python_script.sh ../my_mx3_files/FeGaB_koerner_01_amp_series_${i}.out amp_series_01/ ; done
-#sbatch ../sbatch_scripts/sbatch_run_python_script.sh ../my_mx3_files/FeGaB_koerner_01_B_series_018.out B_series_01/
-#sbatch ../sbatch_scripts/sbatch_run_python_script.sh ../my_mx3_files/FeGaB_koerner_01_B_series_019.out B_series_01/
-#sbatch ../sbatch_scripts/sbatch_run_python_script.sh ../my_mx3_files/FeGaB_koerner_01_B_series_022.out B_series_01/
-#for i in {007..013} ; do sbatch ../sbatch_scripts/sbatch_run_python_script.sh ../my_mx3_files/FeGaB_koerner_01_B_series_${i}.out B_series_01/ ; done
-#sbatch ../sbatch_scripts/sbatch_run_python_script.sh ../my_mx3_files/FeGaB_koerner_01_B_series_015.out B_series_01/
-#sbatch ../sbatch_scripts/sbatch_run_python_script.sh ../my_mx3_files/FeGaB_koerner_01_B_series_007.out B_series_01/
-#sbatch ../sbatch_scripts/sbatch_run_python_script.sh ../my_mx3_files/FeGaB_koerner_01_B_series_011.out B_series_01/
-#sbatch ../sbatch_scripts/sbatch_run_python_script.sh ../my_mx3_files/FeGaB_koerner_01_amp_series_006.out/ amp_series_01/
-sbatch ../sbatch_scripts/sbatch_run_python_script.sh ../my_mx3_files/FeGaB_koerner_01_amp_series_020.out/ amp_series_01/
+for i in {000..049}
+    do sbatch -J $i_avg_amp_series_02 ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_02_out/FeGaB_koerner_02_amp_series_$i.out/ amp_series_02/
+done
