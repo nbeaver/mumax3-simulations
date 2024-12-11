76cf18c26a2e6777bb5431095fe77692ddc4fec9
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-12-11 10:48:28 -0500

    Re-run the ones that ran out of memory.

--- analysis/run.sh
+++ analysis/run.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
-for i in {000..049}
-    do sbatch -J $i_avg_amp_series_02 ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_02_out/FeGaB_koerner_02_amp_series_$i.out/ amp_series_02/
-done
+#for i in {000..049}
+#    do sbatch -J "${i}_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_${i}.out/ amp_series_03/
+#done
+sbatch -J "003_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_003.out/ amp_series_03/
+sbatch -J "008_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_008.out/ amp_series_03/
+sbatch -J "012_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_012.out/ amp_series_03/
+sbatch -J "018_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_018.out/ amp_series_03/
+sbatch -J "019_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_019.out/ amp_series_03/
+sbatch -J "025_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_025.out/ amp_series_03/
+sbatch -J "026_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_026.out/ amp_series_03/
+sbatch -J "027_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_027.out/ amp_series_03/
+sbatch -J "029_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_029.out/ amp_series_03/
+sbatch -J "030_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_030.out/ amp_series_03/
+sbatch -J "032_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_032.out/ amp_series_03/
+sbatch -J "034_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_034.out/ amp_series_03/
+sbatch -J "036_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_036.out/ amp_series_03/
+sbatch -J "039_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_039.out/ amp_series_03/
+sbatch -J "047_avg_amp_series_03" ../sbatch_scripts/sbatch_run_python_script.sh /scratch/n.beaver/amp_series_03_out/FeGaB_koerner_02_amp_series_047.out/ amp_series_03/
