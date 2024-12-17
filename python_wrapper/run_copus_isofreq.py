d2b1e2dcd6ebd32fc0009139b453af839c233226
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2024-12-17 11:30:34 -0500

    Try both sides -1.

--- python_wrapper/run_copus_isofreq.py
+++ python_wrapper/run_copus_isofreq.py
@@ -11,7 +11,7 @@ freq  = float(sys.argv[2])
 print("index = '{}'".format(index))
 print("freq = '{}'".format(freq))
 #working_dir = "/work/sglabfiles/nathaniel/mumax3-simulations/amp_series_out"
-working_dir = "/scratch/n.beaver/18_copus_isofreq_permalloy"
+working_dir = "/scratch/n.beaver/19_copus_isofreq_permalloy"
 os.makedirs(working_dir, exist_ok=True)
 os.chdir(working_dir)
 simname = "copus_isofreq_permalloy_{:03d}".format(index)
@@ -77,8 +77,8 @@ m = uniform(1,0,0)
 // Remove surface charges from left (mx=1) and right (mx=-1) sides.
 // Do this before running relax().
 BoundaryRegion := 0
-MagLeft        := 1
-MagRight       := 1
+MagLeft        := -1
+MagRight       := -1
 ext_rmSurfaceCharge(BoundaryRegion, MagLeft, MagRight)
 
 // relax M to x direction
