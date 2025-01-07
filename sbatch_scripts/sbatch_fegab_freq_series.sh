138066ac16150b7f10a4ad023d31aa2a41e6663a
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2025-01-07 12:56:28 -0500

    Bump up to the maximum time (8 hours).

--- sbatch_scripts/sbatch_fegab_freq_series.sh
+++ sbatch_scripts/sbatch_fegab_freq_series.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --time=4:00:00
+#SBATCH --time=8:00:00
 #SBATCH --job-name=FeGaB_freq_series
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
